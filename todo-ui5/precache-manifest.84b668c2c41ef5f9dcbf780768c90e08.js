84ecf657ba41f1a4cf889610817ef8f551f6a6c3
Jeff Li <jeff.li03@sap.com>
2020-04-14 17:39:17 +0800

    add todo with UI5

---
+++ todo-ui5/precache-manifest.84b668c2c41ef5f9dcbf780768c90e08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caaf5ad3180ec07b7d6ccac232b52c2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bdf1100e8e9fe8be020",
+    "url": "/static/css/2.6010d4dc.chunk.css"
+  },
+  {
+    "revision": "6fc877fcb594390cba94",
+    "url": "/static/css/main.316ebc4c.chunk.css"
+  },
+  {
+    "revision": "9bdf1100e8e9fe8be020",
+    "url": "/static/js/2.bbc5314e.chunk.js"
+  },
+  {
+    "revision": "79a4393ddea5ee19694c6be0f761c68e",
+    "url": "/static/js/2.bbc5314e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc877fcb594390cba94",
+    "url": "/static/js/main.4246e30e.chunk.js"
+  },
+  {
+    "revision": "60b93b34035d52e67097",
+    "url": "/static/js/runtime-main.3697cc02.js"
+  }
+]);
